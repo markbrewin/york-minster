8b3f8634af4d13e7f585f76f8eb8603f67108019
Mark <mark.brewin98@gmail.com>
2018-03-14 12:49:44 +0000

    Moved asset location and added test code.

--- app/src/main/assets/js/minster.js
+++ app/src/main/assets/js/minster.js
@@ -2,5 +2,26 @@ var World = {
     loaded: false,
     
     init: function initFn() {
+        objectTest();
     },
+    
+    objectTest : function objectTestFn(){
+        var objLocation = new AR.RelativeLocation(null, 5, 0, 0);
+
+        var chest = new AR.Model("assets/models/chest.wt3", {
+            scale: {
+                x: 1,
+                y: 1,
+                z: 1
+            }
+        });
+
+        var obj = new AR.GeoObject(objLocation, {
+            drawables: {
+                cam: [chest]
+            }
+        });
+    }
+};
+
 World.init();

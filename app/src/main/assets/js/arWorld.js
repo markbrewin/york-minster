867f15cbda08bcb7cce48bade6700fce0ee6eeee
Mark <mark.brewin98@gmail.com>
2018-03-26 15:10:15 +0100

    Begun loading chests from the database
    Started work on loading the chest locations from the database.
    Changed the datatype of the long, lat, and alt.
    Also changed the menu slightly and removed the top bar from the app.

--- app/src/main/assets/js/arWorld.js
+++ app/src/main/assets/js/arWorld.js
@@ -12,24 +12,4 @@ var World = {
     }
 };
 
-var menu = {
-    init: function initFn() {
-        var menuLocation = new AR.RelativeLocation(null, 5, 0, 0);
-        
-        var htmlDrawable = new AR.HtmlDrawable({
-            uri:"menu.html"}, 5, {
-            onLoaded : World.worldLoaded,
-            clickThroughEnabled: true,
-            viewportHeight: 1024,
-            viewportWidth: 1024
-        });
-        
-        var menu = new AR.GeoObject(menuLocation, {
-            drawables: {
-                cam : [htmlDrawable]
-            }
-        });
-    }
-}
-
 World.init();

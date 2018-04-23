147de8440f4a93ab56d108cc5a40b2c0896c1e2c
Mark <mark.brewin98@gmail.com>
2018-04-23 21:39:00 +0100

    Chest Spawning Idea Changed

--- app/src/main/assets/js/arWorld.js
+++ app/src/main/assets/js/arWorld.js
@@ -1,35 +1,63 @@
 var World = {
     loaded: false,
     
-    chestList: [],
-    
+    chestLocationList: [],
+
     chestModel: null,
+    curChest: null,
+    
+    plyLat: null,
+    plyLong: null,
         
     init: function initFn() {
-        World.chestList = []
+        World.chestLocationList = [];
+        
         World.chestModel = new AR.Model("assets/models/chest.wt3", {
             scale: {
-                x: 1,
-                y: 1,
-                z: 1
+                x: 0.05,
+                y: 0.05,
+                z: 0.05
             }
         });
+        
+        World.indicatorImage = new AR.ImageResource("assets/indi.png");
+        
+        World.inidcatorDrawable = new AR.ImageDrawable(World.indicatorImage, 0.1, {
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
+        }); 
+        
+        AR.context.onLocationChanged = function(latitude, longitude, altitude, accuracy){
+            World.plyLat = latitude;
+            World.plyLong = longitude;
+            
+            console.log("update");
+        
+            document.getElementById("loadingMessage").innerHTML = World.curChest.locations[0].latitude + "," + World.curChest.locations[0].longitude + "<br>" + World.plyLat + "," + World.plyLong;
+        };
     },
     
-    addChest: function addChest(lat, long, alt) {
-        var location = new AR.GeoLocation(lat, long, alt);
-        var indicatorImage = new AR.ImageResource("assets/indi.png");
+    addChest: function addChestFn(lat, long, alt) {
+        //World.chestList.push(new chest(lat, long, alt));
+    },
     
-        var indicatorDrawable = new AR.ImageDrawable(indicatorImage, 0.1, {
-            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
+    loadChest: function loadChestFn(){
+        var location = new AR.RelativeLocation(null, -50, 0, 0);
+        
+        World.curChest = new AR.GeoObject(location, {
+                drawables: {
+                cam: [World.chestModel],
+                indicator: [World.inidcatorDrawable]
+            } 
         });
-        var chestGeoLocation = new AR.GeoObject(location, {
-           drawables: {
-               cam: [World.chestModel],
-               indicator: [indicatorDrawable]
-           } 
-        });
-        World.chestList.push(chestGeoLocation);
+        
+        World.rotateChest();
+    },
+    
+    rotateChest: function rotateChestFn(){
+        console.log("rotate");
+
+        World.curChest.drawables.cam[0].rotate.heading += 1;
+        setTimeout(World.rotateChest(), 5000);
     },
     
     worldLoaded: function worldLoadedFn() {
